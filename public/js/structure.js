6730e922b0a82ad421623a597da07784e5edcf6c
rixquli <81580332+rixquli@users.noreply.github.com>
2022-11-12 22:53:44 +0100

    Add files via upload

---
+++ public/js/structure.js
@@ -0,0 +1,77 @@
+//let random = Math.floor(Math.random() * 100);
+if (
+  !this.seedRandom[this.structureSeed] ||
+  this.seedRandom[this.structureSeed] == NaN
+) {
+  this.addSeedRandom();
+}
+let random = this.randomNumber(this.seedRandom[this.structureSeed]);
+//function worldGen() {
+//    let worldInGen = world();
+//    return worldInGen * 10 ** (worldInGen.toString().length - 2);
+//  }
+//
+//let random = worldGen()
+
+if (random < this.randomP.houseVillager) {
+  this.createFloor(
+    (Ammo = this.AmmoClone),
+    x,
+    z,
+    0x00ff00,
+    "img/grass.png",
+    "houseVillager"
+  );
+
+  this.loadStructure("house_minecraft/scene.gltf", x, z, 20);
+  //this.createVillager(Ammo = AmmoClone, x,z)
+} else if (random < this.randomP.stone) {
+  console.log("stone");
+  this.createFloor(
+    (Ammo = this.AmmoClone),
+    x,
+    z,
+    0xcccccc,
+    "img/Andesite.png",
+    "stone"
+  );
+  this.loadStructure("stylized__rock/scene.gltf", x, z, 40);
+} else if (random < this.randomP.forest) {
+  this.createFloor(
+    (Ammo = this.AmmoClone),
+    x,
+    z,
+    0xffea33,
+    "img/grass.png",
+    "forest"
+  );
+  this.loadStructure("a_simple_tree/scene.gltf", x, z, 0.01);
+} else if (random < this.randomP.farmWithWheat) {
+  this.createFloor(
+    (Ammo = this.AmmoClone),
+    x,
+    z,
+    0xffea33,
+    "https://static.wikia.nocookie.net/minecraft_gamepedia/images/3/37/Dirt_Path_%28top_texture%29_JE2_BE2.png",
+    "wheat"
+  );
+  //this.loadStructure("source/Field of wheat--eWhaaKNh755/FieldOfWheat(1).gltf", x, z, 1.2);
+} else if (random < this.randomP.farmWithoutWheat) {
+  this.createFloor(
+    (Ammo = this.AmmoClone),
+    x,
+    z,
+    0xffea33,
+    "https://static.wikia.nocookie.net/minecraft_gamepedia/images/3/37/Dirt_Path_%28top_texture%29_JE2_BE2.png"
+  );
+} else {
+  this.createFloor(
+    (Ammo = this.AmmoClone),
+    x,
+    z,
+    0x00ff00,
+    "img/grass.png",
+    "plain"
+  );
+}
+this.structureSeed++;
---
+++ socket.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const path = require("path");
+const port = 3000;
+const io = require("socket.io")(http, {
+  cors: { originF: "*" },
+});
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index with bos copy socket.html");
+});
+
+class ConnectedUser {
+  constructor(socket) {
+    this.id = socket.id;
+    this.pos = [
+      Math.floor(Math.random() * 20),
+      19,
+      Math.floor(Math.random() * 20),
+    ];
+    this.socket = socket;
+    this.socket.on("pos", (d) => {
+      this.pos = [...d];
+      this.spamEveryone();
+    });
+    this.spamEveryone();
+  }
+  spamEveryone() {
+    this.socket.emit("pos", [this.id, this.pos]);
+
+    USERS.forEach((e) => {
+      e.socket.emit("pos", [this.id, this.pos]);
+      this.socket.emit("pos", [e.id, e.pos]);
+    });
+  }
+}
+
+let USERS = [];
+
+io.on("connection", (socket) => {
+  console.log("login");
+  socket.on("newPlayer", () => {
+    USERS[socket.id]=new ConnectedUser(socket)
+  });
+  socket.on("disconnect", () => {
+    console.log("disconnect");
+    delete USERS[socket.id]
+    USERS.forEach((e) => {
+      e.socket.emit("deletePlayer", socket.id);
+      //this.socket.emit("pos", [e.id, e.pos]);
+    });
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}/`);
+});
