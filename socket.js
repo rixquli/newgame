4e922376fc7ed8a2b111560caf8113f1929a753c
rixquli <81580332+rixquli@users.noreply.github.com>
2022-11-12 22:59:11 +0100

    Add files via upload

--- socket.js
+++ socket.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const io = require("socket.io")(http, {
   cors: { originF: "*" },
 });
